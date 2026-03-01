65a3c9c13d4e4668667c60a9805c764c8781fbc7
me1lkom <melkom06@gmail.com>
2026-03-01 23:34:20 +0300

    practice3 done

--- practice2/app.js
+++ practice2/app.js
@@ -93,15 +93,16 @@ app.post('/goods', (req, res) => {
         price
     };
 
-    res.push(newGoods);
-res});
+    goods.push(newGoods);
+    res.status(201).json(newGoods);
+});
 
 app.get('/goods', (req, res) => {
     res.send(JSON.stringify(goods));
 });
 
 app.get('/goods/:id', (req, res) => {
-    let product = goods.find(u => u.id == res.params.id);
+    let product = goods.find(u => u.id == req.params.id);
     res.send(JSON.stringify(product));
 });
 
@@ -117,7 +118,7 @@ app.patch('/goods/:id', (req, res) => {
 
 app.delete('/goods/:id', (req, res) => {
     goods = goods.filter(u => u.id != req.params.id);
-    res.send('ok');
+    res.send('Goods is deleted!');
 });
 
 app.listen(port, () => {
