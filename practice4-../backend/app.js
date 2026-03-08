78fb749db028919a11d87d3a182f6598d4644f46
me1lkom <melkom06@gmail.com>
2026-03-08 19:42:34 +0300

    practice8

--- practice4-../backend/app.js
+++ practice4-../backend/app.js
@@ -9,6 +9,10 @@ const swaggerUi = require('swagger-ui-express');
 const app = express();
 const port = 3000;
 
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'access_secret';
+const ACCESS_EXPIRES_IN = '15m';
+
 const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
@@ -23,22 +27,23 @@ const swaggerOptions = {
         description: 'Локальный сервер',
       },
     ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        }
+      }
+    }
   },
-  // Путь к файлам с JSDoc-комментариями
-  apis: ['./app.js'], // указываем текущий файл
+  apis: ['./app.js'],
 };
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 let users = [];
-function findUserOr404(email, res) {
-  const user = users.find(u => u.email == email);
-  if (!user) {
-    res.status(404).json({ error: "user not found" });
-    return null;
-  }
-  return user;
-}
+
 async function hashPassword(password) {
   const rounds = 10;
   return bcrypt.hash(password, rounds);
@@ -62,9 +67,31 @@ ${res.statusCode} ${req.path}`);
 // Настройка CORS (разрешаем запросы с фронтенда)
 app.use(cors({
   origin: "http://localhost:3001",
-  methods: ["GET", "POST", "PATCH", "DELETE"],
+  methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
+
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+
+  const [scheme, token] = authHeader.split(' ');
+
+  if (scheme !== 'Bearer' || !token) {
+    return res.status(401).json({ error: 'Invalid token format' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded; // { sub: userId, email: user.email, iat, exp }
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Invalid or expired token' });
+  }
+}
 
 
 /**
@@ -108,7 +135,7 @@ app.use(cors({
  *               properties:
  *                 id:
  *                   type: string
- *                   example: ab12cd
+ *                   example: 1
  *                 email:
  *                   type: string
  *                   example: primeremail@gmail.com
@@ -126,6 +153,7 @@ app.use(cors({
  */
 app.post("/api/auth/register", async (req, res) => {
   const { email, first_name, last_name, password } = req.body;
+
   if (!email || !first_name || !last_name || !password) {
     return res.status(400).json({ error: "email, first_name, last_name and password are required" });
   }
@@ -136,7 +164,7 @@ app.post("/api/auth/register", async (req, res) => {
   }
 
   const newUser = {
-    id: nanoid(6),
+    id: String(users.length + 1),
     email: email,
     first_name: first_name,
     last_name: last_name,
@@ -144,7 +172,13 @@ app.post("/api/auth/register", async (req, res) => {
   };
 
   users.push(newUser);
-  res.status(201).json(newUser);
+
+  res.status(201).json({
+    id: newUser.id,
+    email: newUser.email,
+    first_name: newUser.first_name,
+    last_name: newUser.last_name,
+  });
 });
 
 
@@ -175,14 +209,14 @@ app.post("/api/auth/register", async (req, res) => {
  *     responses:
  *       200:
  *         description: Успешная авторизация
- *         content:
+  *         content:
  *           application/json:
  *             schema:
  *               type: object
  *               properties:
- *                 login:
- *                   type: boolean
- *                   example: true
+ *                 accessToken:
+ *                   type: string
+ *                   example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
  *       400:
  *         description: Отсутствуют обязательные поля
  *       401:
@@ -193,19 +227,88 @@ app.post("/api/auth/register", async (req, res) => {
 
 app.post("/api/auth/login", async (req, res) => {
   const { email, password } = req.body;
+
   if (!email || !password) {
     return res.status(400).json({ error: "email and password are required" });
   }
-  const user = findUserOr404(email, res);
-  if (!user) return;
-  isAuthenticated = await verifyPassword(password, user.hashedPassword);
-  if (isAuthenticated) {
-    res.status(200).json({ login: true });
+
+  const user = users.find(u => u.email === email);
+  if (!user) {
+    return res.status(401).json({
+      error: "Invalid credentials",
+    });
   }
-  else {
-    res.status(401).json({ error: "not authenticated" })
+
+  const isValid = await bcrypt.compare(password, user.hashedPassword);
+  if (!isValid) {
+    return res.status(401).json({
+      error: "Invalid credentials",
+    });
   }
+
+  const accessToken = jwt.sign(
+    {
+      sub: user.id,
+      email: user.email,
+      first_name: user.first_name,
+      last_name: user.last_name
+    },
+    JWT_SECRET,
+    { expiresIn: ACCESS_EXPIRES_IN }
+  );
+
+  res.json({
+    accessToken,
+  });
+
 });
+
+/**
+ * @swagger
+ * /api/auth/me:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Возвращает данные текущего пользователя
+ *     description: Получает информацию о пользователе из JWT токена
+ *     tags: [Auth]
+ *     responses:
+ *       200:
+ *         description: Пользователь найден
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                   example: "1"
+ *                 email:
+ *                   type: string
+ *                   example: "primeremail@gmail.com"
+ *       401:
+ *         description: Неверные учетные данные
+ *       404:
+ *         description: Пользователь не найден
+ */
+app.get("/api/auth/me", authMiddleware, (req, res) => {
+  const userId = req.user.sub;
+
+  const user = users.find(u => u.id === userId);
+
+  if (!user) {
+    return res.status(404).json({
+      error: "User not found",
+    });
+  }
+
+  res.json({
+    id: user.id,
+    email: user.email
+  });
+});
+
+
 
 let products = [
   {
@@ -395,6 +498,8 @@ app.get("/api/products", (req, res) => {
  * @swagger
  * /api/products/{id}:
  *   get:
+ *     security:
+ *       - bearerAuth: []
  *     summary: Получает товар по ID
  *     tags: [Products]
  *     parameters:
@@ -420,10 +525,12 @@ app.get("/api/products", (req, res) => {
  *               $ref: '#/components/schemas/Error'
  */
 // GET /api/products/:id - получить товар по ID
-app.get("/api/products/:id", (req, res) => {
+app.get("/api/products/:id", authMiddleware, (req, res) => {
   const id = req.params.id;
   const product = findProductOr404(id, res);
   if (!product) return;
+
+  console.log(`Товар найден пользователем: ${req.user.email}`);
   res.json(product);
 });
 
@@ -484,6 +591,10 @@ app.get("/api/products/:id", (req, res) => {
 app.post("/api/products", (req, res) => {
   const { name, category, description, price, stock, imageUrl } = req.body;
 
+  if (!name || !category || !description || !price || !stock || !imageUrl) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
   const newProduct = {
     id: nanoid(6),
     name: name?.trim(),
@@ -501,7 +612,9 @@ app.post("/api/products", (req, res) => {
 /**
  * @swagger
  * /api/products/{id}:
- *   patch:
+ *   put:
+ *     security:
+ *       - bearerAuth: []
  *     summary: Обновляет существующий товар
  *     tags: [Products]
  *     parameters:
@@ -548,8 +661,8 @@ app.post("/api/products", (req, res) => {
  *       404:
  *         description: Товар не найден
  */
-// PATCH /api/products/:id - обновить товар
-app.patch("/api/products/:id", (req, res) => {
+// PUT /api/products/:id - обновить товар
+app.put("/api/products/:id", authMiddleware, (req, res) => {
   const id = req.params.id;
   const product = findProductOr404(id, res);
   if (!product) return;
@@ -567,6 +680,7 @@ app.patch("/api/products/:id", (req, res) => {
   if (stock !== undefined) product.stock = Number(stock);
   if (imageUrl !== undefined) product.imageUrl = imageUrl.trim();
 
+  console.log(`Товар обновлен пользователем: ${req.user.email}`);
   res.json(product);
 });
 
@@ -574,6 +688,8 @@ app.patch("/api/products/:id", (req, res) => {
  * @swagger
  * /api/products/{id}:
  *   delete:
+ *     security:
+ *       - bearerAuth: []       
  *     summary: Удаляет товар
  *     tags: [Products]
  *     parameters:
@@ -594,12 +710,13 @@ app.patch("/api/products/:id", (req, res) => {
  *               $ref: '#/components/schemas/Error'
  */
 // DELETE /api/products/:id - удалить товар
-app.delete("/api/products/:id", (req, res) => {
+app.delete("/api/products/:id", authMiddleware, (req, res) => {
   const id = req.params.id;
   const exists = products.some(p => p.id === id);
   if (!exists) return res.status(404).json({ error: "Product not found" });
 
   products = products.filter(p => p.id !== id);
+  console.log(`Товар удален пользователем: ${req.user.email}`);
   res.status(204).send();
 });
 
