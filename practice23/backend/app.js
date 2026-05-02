de332d22f2fd71b2494c40acde2ea7c370db1358
me1lkom <melkom06@gmail.com>
2026-05-02 20:52:26 +0300

    practice23

---
+++ practice23/backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const SERVER_ID = process.env.SERVER_ID;
+
+app.get('/', (req, res) => {
+    res.json({
+        server: `backend-${SERVER_ID}`,
+        port: PORT
+    });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Сервер ${SERVER_ID} запущен на http://localhost:${PORT}`);
+})
